1b28a866a17d5d79ca2219d28be782379d01da36
olzich <olzich2@gmail.com>
2023-03-06 20:07:09 +0200

    create template file index.html and match it with views.py

--- countries/country_info/views.py
+++ countries/country_info/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import countryinfo
+
 
 def index(requests):
-    return HttpResponse('Hi')
+    country = countryinfo.CountryInfo("Aruba")
+    info = country.info()
+    key_list = info.values()
+    return render(requests, 'country_info/index.html', context={'title': "Info about country", 'info': info, 'key_list': key_list})
